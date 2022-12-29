6995cf5c76fdd761444be5ce2e59bd7cc3792677
Attulaya <attulaya@gmail.com>
2022-12-29 18:13:23 +0530

    5th commit

--- Prefixtopostfix.c
+++ Prefixtopostfix.c
@@ -1,47 +1,50 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-# define MAX 20
-char str[MAX],stack[MAX];
-int top=-1;
-void push(char c) //to push the character in to the stack
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX 20
+char str[MAX], stack[MAX];
+int top = -1;
+void push(char c) // to push the character in to the stack
 {
-   stack[++top]=c;
+   stack[++top] = c;
 }
 char pop()
 {
    return stack[top--];
 }
-void pre_post()//function to convert prefix to postfix
+void pre_post() // function to convert prefix to postfix
 {
-   int n,i,j=0; char c[20];
+   int n, i, j = 0;
+   char c[20];
    char a;
    printf("Enter the prefix expression\n");
-   gets(str);// used to scan the prefix expression
-   n=strlen(str);
-   for(i=0;i<MAX;i++)
-   stack[i]='\0';
+   gets(str); // used to scan the prefix expression
+   n = strlen(str);
+   for (i = 0; i < MAX; i++)
+      stack[i] = '\0';
    printf("Postfix expression is:\t");
-   for(i=0;i<n;i++)
+   for (i = 0; i < n; i++)
    {
-      if(str[i]=='+'||str[i]=='-'||str[i]=='*'||str[i]=='/')
+      if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
       {
          push(str[i]);
       }
       else
-      { c[j++]=str[i];
-        while((top!=-1)&&(stack[top]=='#'))
-        {
-            a=pop();
-             c[j++]=pop();
-        }
-        push('#');
+      {
+         c[j++] = str[i];
+         while ((top != -1) && (stack[top] == '#'))
+         {
+            a = pop();
+            c[j++] = pop();
+         }
+         push('#');
       }
    }
-   c[j]='\0';
-   printf("%s",c);
+   c[j] = '\0';
+   printf("%s", c);
 }
-main()
+int main()
 {
-    pre_post();
+   pre_post();
+   return 0;
 }
